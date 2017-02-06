17adbb8d649b988cf0a7e3dccdb97f2b719bd2e5
Mark McDowall <markus.mcd5@gmail.com>
2017-02-06 00:29:51 -0800

    Fix TableRowCellButton styling

--- frontend/src/Components/Table/Cells/TableRowCellButton.js
+++ frontend/src/Components/Table/Cells/TableRowCellButton.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Link from 'Components/Link/Link';
 import TableRowCell from './TableRowCell';
+import styles from './TableRowCellButton.css';
 
-function TableRowCellButton(props) {
+function TableRowCellButton({ className, ...otherProps }) {
   return (
     <Link
+      className={className}
       component={TableRowCell}
-      {...props}
+      {...otherProps}
     />
   );
 }
+
+TableRowCellButton.propTypes = {
+  className: PropTypes.string.isRequired
+};
+
+TableRowCellButton.defaultProps = {
+  className: styles.cell
+};
 
 export default TableRowCellButton;
